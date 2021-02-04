fabe7681180c5e07850cf94373bc1a303ad1f315
github username <github email address>
2021-02-04 16:03:39 -0600

    submitted

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
+const URL = 'http://localhost:3000/toys/'
+
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
   }
 
   handleClick = () => {
@@ -19,20 +22,65 @@ class App extends React.Component{
     })
   }
 
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(toyArray => this.setState({toys: toyArray}))
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const newToy = {
+      name: event.target.name.value,
+      image: event.target.image.value,
+      likes: 0
+    }
+
+    const reqPackage = {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(newToy)
+    }
+
+    fetch(URL, reqPackage)
+      .then(res => res.json())
+      .then(newToy => {
+        this.setState({
+          toys: [...this.state.toys, newToy]
+        })
+      })
+
+    event.target.reset()
+  }
+
+  handleDelete = (toyObj) => {
+    fetch(URL + toyObj.id, {method: "DELETE"})
+      .then(res => res.json())
+      .then(() => {
+        this.setState({
+          toys: this.state.toys.filter(toy => toy != toyObj)
+        })
+      })
+      
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm handleSubmit={this.handleSubmit}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toysArray={this.state.toys} handleDelete={this.handleDelete}/>
       </>
     );
   }
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -5,11 +5,11 @@ class ToyCard extends Component {
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={'' /* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
+        <h2>{this.props.toyInfo.name}</h2>
+        <img src={this.props.toyInfo.image} alt={this.props.toyInfo.name} className="toy-avatar" />
+        <p>{this.props.toyInfo.likes} Likes </p>
         <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <button className="del-btn" onClick={() => this.props.handleDelete(this.props.toyInfo)}>Donate to GoodWill</button>
       </div>
     );
   }
